49e7e236644c503e649628c46a2ab52f58e366ab
John K. Grant <john@fabric.space>
2023-05-20 23:34:34 -0700

    build: :arrow_up: update for aframe 1.4 which uses THREE r147

--- dist/aframe-gif-shader.js
+++ dist/aframe-gif-shader.js
@@ -543,8 +543,8 @@
 	    this.__frames = frames;
 	    this.__frameCnt = times.length;
 	    this.__startTime = Date.now();
-	    this.__width = THREE.Math.floorPowerOfTwo(frames[0].width);
-	    this.__height = THREE.Math.floorPowerOfTwo(frames[0].height);
+	    this.__width = THREE.MathUtils.floorPowerOfTwo(frames[0].width);
+	    this.__height = THREE.MathUtils.floorPowerOfTwo(frames[0].height);
 	    this.__cnv.width = this.__width;
 	    this.__cnv.height = this.__height;
 	    this.__draw();
